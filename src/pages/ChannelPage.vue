0282af48d9cbe9ad182418753f0ea80d5fc10eaf
JSimko4 <xsimkoj2@stuba.sk>
2022-03-26 15:09:28 +0100

    infinite scroll

--- src/pages/ChannelPage.vue
+++ src/pages/ChannelPage.vue
@@ -1,34 +1,42 @@
 <template>
   <div class="q-pa-md q-mt-sm row justify-center">
     <div style="width: 95%">
-      <q-chat-message
-        name="Martin"
-        avatar="https://cdn.quasar.dev/img/avatar4.jpg"
-        bg-color="red"
-      >
-        <div>@Jakub tagnuta sprava je červená</div>
-      </q-chat-message>
+      <q-infinite-scroll @load="onLoad" :offset="10">
+        <template v-slot:loading>
+          <div class="row justify-center q-my-md">
+            <q-spinner color="primary" name="dots" size="40px" />
+          </div>
+        </template>
 
-      <message-component
-        v-for="(message, index) in messages"
-        :key="index"
-        :isSender="message.isSender"
-        :messagerName="message.messagerName"
-        :avatar="message.avatar"
-        :text="message.text"
-        :stamp="message.stamp"
-      />
+        <q-chat-message
+          name="Martin"
+          avatar="https://cdn.quasar.dev/img/avatar4.jpg"
+          bg-color="red"
+        >
+          <div>@Jakub tagnuta sprava je červená</div>
+        </q-chat-message>
 
-      <q-chat-message
-        name="Martin"
-        avatar="https://cdn.quasar.dev/img/avatar4.jpg"
-        bg-color="amber"
-      >
-        <div>
-          Martin is typing
-          <q-spinner-dots class="q-ml-sm" size="2rem" />
-        </div>
-      </q-chat-message>
+        <message-component
+          v-for="(message, index) in messages"
+          :key="index"
+          :isSender="message.isSender"
+          :messagerName="message.messagerName"
+          :avatar="message.avatar"
+          :text="message.text"
+          :stamp="message.stamp"
+        />
+
+        <q-chat-message
+          name="Martin"
+          avatar="https://cdn.quasar.dev/img/avatar4.jpg"
+          bg-color="amber"
+        >
+          <div>
+            Martin is typing
+            <q-spinner-dots class="q-ml-sm" size="2rem" />
+          </div>
+        </q-chat-message>
+      </q-infinite-scroll>
     </div>
   </div>
 </template>
@@ -50,6 +58,29 @@ export default defineComponent({
       set(messages: Message[]) {
         this.$store.commit('message/setMessages', messages);
       },
+    },
+  },
+
+  methods: {
+    onLoad(index: number, done: (arg0: boolean) => void) {
+      setTimeout(() => {
+        let new_message = {
+          isSender: false,
+          messagerName: 'Martin',
+          avatar: 'https://cdn.quasar.dev/img/avatar4.jpg',
+          text: 'xxx sprava test 123 ',
+          stamp: '18.3.2022 at 12:22',
+        };
+
+        for (var i = 0; i < 5; i++) {
+          this.$store.commit('message/addMessage', new_message);
+        }
+
+        let isDone = false;
+        if (this.messages.length > 10) isDone = true;
+
+        done(isDone);
+      }, 2000);
     },
   },
 });
